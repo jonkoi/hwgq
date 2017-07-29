77380d2e51a9cc01686e56d187066152505ec402
Yaman Umuroglu <maltanar@gmail.com>
2017-07-29 21:01:35 +0200

    try to optimize im2row a little

--- src/caffe/layers/integer_convolution_layer.cpp
+++ src/caffe/layers/integer_convolution_layer.cpp
@@ -81,40 +81,40 @@ void IntegerConvolutionLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
 
 // from DarkNet
 template <typename Dtype>
-inline float im2col_get_pixel(const Dtype *im, int height, int width, int channels,
-                        int row, int col, int channel, int pad)
+inline float im2row_get_pixel(const Dtype *im, const int height, const int width, const int channels,
+                        const int row, const int col, const int channel, const int pad)
 {
-    row -= pad;
-    col -= pad;
+    const int prow = row - pad;
+    const int pcol = col - pad;
 
-    if (row < 0 || col < 0 ||
-        row >= height || col >= width) return 0;
-    return im[col + width*(row + height*channel)];
+    if (prow < 0 || pcol < 0 ||
+        prow >= height || pcol >= width) return 0;
+    return im[pcol + width*(prow + height*channel)];
 }
 
 //From Berkeley Vision's Caffe!
 //https://github.com/BVLC/caffe/blob/master/LICENSE
 template <typename Dtype>
-void darknet_im2col_cpu(const Dtype* data_im,
-     int channels,  int height,  int width,
-     int ksize,  int stride, int pad, Dtype* data_col)
+void darknet_im2row_cpu(const Dtype* data_im,
+     const int channels, const int height, const int width,
+     const int ksize, const int stride, const int pad, Dtype* data_col)
 {
     int c,h,w;
-    int height_col = (height + 2*pad - ksize) / stride + 1;
-    int width_col = (width + 2*pad - ksize) / stride + 1;
+    const int height_col = (height + 2*pad - ksize) / stride + 1;
+    const int width_col = (width + 2*pad - ksize) / stride + 1;
+    const int k2 = ksize * ksize;
 
     int channels_col = channels * ksize * ksize;
     for (c = 0; c < channels_col; ++c) {
-        int w_offset = c % ksize;
-        int h_offset = (c / ksize) % ksize;
-        int c_im = c / ksize / ksize;
+        const int w_offset = c % ksize;
+        const int h_offset = (c / ksize) % ksize;
+        const int c_im = c / k2;
         for (h = 0; h < height_col; ++h) {
             for (w = 0; w < width_col; ++w) {
-                int im_row = h_offset + h * stride;
-                int im_col = w_offset + w * stride;
-                //int col_index = (c * height_col + h) * width_col + w;
-                int col_index = c + channels_col * (w + h * width_col);
-                data_col[col_index] = im2col_get_pixel(data_im, height, width, channels,
+                const int im_row = h_offset + h * stride;
+                const int im_col = w_offset + w * stride;
+                const int col_index = c + channels_col * (w + h * width_col);
+                data_col[col_index] = im2row_get_pixel(data_im, height, width, channels,
                         im_row, im_col, c_im, pad);
             }
         }
@@ -154,8 +154,11 @@ void IntegerConvolutionLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bot
       const uint8_t * in_buff_u8 = ((uint8_t*)(bottom[0]->cpu_data())) + m_ifm * m_indim * m_indim * d;
       uint8_t * col_buff_u8 = (uint8_t*)(col_buffer_.mutable_cpu_data());
       TIMER_START
-      im2col_cpu(in_buff_u8, m_ifm, m_indim, m_indim,
-          m_k, m_k, m_pad, m_pad, m_stride, m_stride, 1, 1, col_buff_u8);
+      /*im2col_cpu(in_buff_u8, m_ifm, m_indim, m_indim,
+          m_k, m_k, m_pad, m_pad, m_stride, m_stride, 1, 1, col_buff_u8);*/
+      darknet_im2row_cpu(
+        in_buff_u8, m_ifm, m_indim, m_indim, m_k, m_stride, m_pad, col_buff_u8
+      );
       TIMER_END
       TIMER_GET(uscount_im2col)
       // turn transpose of patch matrix into bit serial form
@@ -173,7 +176,7 @@ void IntegerConvolutionLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bot
       im2col_cpu(in_buff, m_ifm, m_indim, m_indim,
           m_k, m_k, m_pad, m_pad, m_stride, m_stride, 1, 1, col_buff);
       */
-      darknet_im2col_cpu(
+      darknet_im2row_cpu(
         in_buff, m_ifm, m_indim, m_indim, m_k, m_stride, m_pad, col_buff
       );
       TIMER_END
